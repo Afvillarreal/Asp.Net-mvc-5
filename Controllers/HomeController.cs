7a6087928130761c5e15466d939b953b6c86d847
Andres Felipe Villarreal <51982396+Afvillarreal@users.noreply.github.com>
2020-05-10 21:01:52 -0500

    first integration with the controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PROYECTO_MVC.Services;
+
+namespace PROYECTO_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            var PeliculasServices = new PeliculasService();
+            var model = PeliculasServices.Obtenerpeliculas();
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
